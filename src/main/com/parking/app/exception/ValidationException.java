ca50b6ba9be4048b9d9ef0e5ce009b323de47673
Harikrishnan K <78860755+harikrishnan-creator@users.noreply.github.com>
2026-08-28 20:58:02 +0530

    Create ValidationException.java

---
+++ src/main/com/parking/app/exception/ValidationException.java
@@ -0,0 +1,9 @@
+package com.parking.app.exception;
+
+public class ValidationException
+        extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}

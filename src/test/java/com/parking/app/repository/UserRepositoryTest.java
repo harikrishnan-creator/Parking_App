ae148ea9a40f2dc2cf21a4d7590c295f86181ea8
Harikrishnan K <78860755+harikrishnan-creator@users.noreply.github.com>
2026-08-28 22:01:35 +0530

    Create UserRepositoryTest.java

---
+++ src/test/java/com/parking/app/repository/UserRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.parking.app.repository;
+
+import com.parking.app.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    void repositoryShouldBeCreated() {
+        assertNotNull(repository);
+    }
+}

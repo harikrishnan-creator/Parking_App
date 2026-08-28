0fc32d1c7ca00f78eaa88ddbc7e5ad58f938807e
Harikrishnan K <78860755+harikrishnan-creator@users.noreply.github.com>
2026-08-28 20:17:16 +0530

    Create VehicleDTO.java

---
+++ src/main/com/parking/app/DTO/VehicleDTO.java
@@ -0,0 +1,11 @@
+package com.parking.app.dto;
+
+import lombok.Data;
+
+@Data
+public class VehicleDTO {
+
+    private String vehicleNumber;
+    private String vehicleType;
+    private String ownerName;
+}

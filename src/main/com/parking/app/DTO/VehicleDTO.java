daeab5b6515707f453b2a2f8ebfc1381ba1e1f73
Harikrishnan K <78860755+harikrishnan-creator@users.noreply.github.com>
2026-08-28 20:41:00 +0530

    Update VehicleDTO.java

--- src/main/com/parking/app/DTO/VehicleDTO.java
+++ src/main/com/parking/app/DTO/VehicleDTO.java
@@ -5,7 +5,11 @@ import lombok.Data;
 @Data
 public class VehicleDTO {
 
+    private Long id;
+
     private String vehicleNumber;
+
     private String vehicleType;
+
     private String ownerName;
 }

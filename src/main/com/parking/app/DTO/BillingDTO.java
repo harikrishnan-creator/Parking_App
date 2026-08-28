dc397cccffd1f90a2be746777804f0da5e0dcd71
Harikrishnan K <78860755+harikrishnan-creator@users.noreply.github.com>
2026-08-28 20:40:39 +0530

    Update BillingDTO.java

--- src/main/com/parking/app/DTO/BillingDTO.java
+++ src/main/com/parking/app/DTO/BillingDTO.java
@@ -3,12 +3,23 @@ package com.parking.app.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class BillingDTO {
 
     private String tokenNumber;
+
     private String vehicleNumber;
+
+    private LocalDateTime entryTime;
+
+    private LocalDateTime exitTime;
+
     private Long parkedMinutes;
+
     private Double billAmount;
+
+    private String status;
 }

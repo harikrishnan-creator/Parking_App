05d0cac59a9843679bf3251ddcd0c8c80dc83bbe
Harikrishnan K <78860755+harikrishnan-creator@users.noreply.github.com>
2026-08-28 20:18:32 +0530

    Create VehicleRepository.java

---
+++ src/main/com/parking/app/Repository/VehicleRepository.java
@@ -0,0 +1,12 @@
+package com.parking.app.repository;
+
+import com.parking.app.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VehicleRepository
+        extends JpaRepository<Vehicle, Long> {
+
+    Optional<Vehicle> findByVehicleNumber(String vehicleNumber);
+}

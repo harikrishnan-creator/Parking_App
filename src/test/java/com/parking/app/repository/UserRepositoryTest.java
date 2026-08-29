ab1725c88962d17e2d583823d948df05ec8caf57
Harikrishnan K <78860755+harikrishnan-creator@users.noreply.github.com>
2026-08-29 14:04:36 +0530

    Update UserRepositoryTest.java

--- src/test/java/com/parking/app/repository/UserRepositoryTest.java
+++ src/test/java/com/parking/app/repository/UserRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
     @Autowired
